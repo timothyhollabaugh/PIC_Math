9a61a99ea91da7231176368d28c18eef079a95d9
Tim <timothyhollabaugh@gmail.com>
2019-02-19 22:27:55 -0500

    Added function to load images

---
+++ src/load_images.py
@@ -0,0 +1,17 @@
+
+import os
+import cv2
+
+
+def load_images(path):
+    images = []
+
+    if os.path.isdir(path):
+        for filename in sorted(os.listdir(path)):
+            img = cv2.imread(os.path.join(path, filename))
+            images.append(img)
+    else:
+        img = cv2.imread(path)
+        images.append(img)
+
+    return images
