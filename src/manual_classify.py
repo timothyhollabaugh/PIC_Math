435002922bb3a6c5c60c1ec1f1f3cbd0dc4a8d27
Tim <timothyhollabaugh@gmail.com>
2019-03-26 09:08:14 -0400

    Sharpen digits before showing, print names, esc to quit

--- src/manual_classify.py
+++ src/manual_classify.py
@@ -22,35 +22,48 @@ def manual_classify(path):
     outfile = open("ground_truth.csv", 'w')
 
     for path, image in images:
+        filename = os.path.basename(path)
+        print("Image: " + filename)
         (key1, key2, key3) = manual_classify_image(image)
         outfile.write(str(key1) + ", " + str(key2) + ", " + str(key3)
-                      + ", " + os.path.basename(path) + "\n")
+                      + ", " + filename + "\n")
+
+        if key1 == -21 or key2 == -21 or key3 == -21:
+            break
 
     outfile.close()
 
 
 def manual_classify_image(image):
 
-    digit1, digit2, digit3 = cropped.crop_image(image)
+    sharpened = cropped.sharpen(image)
+
+    digit1, digit2, digit3 = cropped.crop_image(sharpened)
 
     # ratio adjustment
     r = 100.0 / digit1.shape[1]
     dim = (100, int(digit1.shape[0] * r))
 
+    print("digit 1")
     digit1 = cv2.resize(digit1, dim, interpolation=cv2.INTER_AREA)
-    cv2.imshow("digit", digit1)
+    cv2.imshow("digit 1", digit1)
     key1 = cv2.waitKey(0) - 48
     print(key1)
+    cv2.destroyAllWindows()
 
+    print("digit 2")
     digit2 = cv2.resize(digit2, dim, interpolation=cv2.INTER_AREA)
-    cv2.imshow("digit", digit2)
+    cv2.imshow("digit 2", digit2)
     key2 = cv2.waitKey(0) - 48
     print(key2)
+    cv2.destroyAllWindows()
 
+    print("digit 3")
     digit3 = cv2.resize(digit3, dim, interpolation=cv2.INTER_AREA)
-    cv2.imshow("digit", digit3)
+    cv2.imshow("digit 3", digit3)
     key3 = cv2.waitKey(0) - 48
     print(key3)
+    cv2.destroyAllWindows()
 
     return key1, key2, key3
 
